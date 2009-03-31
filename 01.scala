01953555ec69463cae4bbac2b6e78820d65dbc22
Paul Barry <mail@paulbarry.com>
2009-03-31 02:37:40 -0400

    Updated to return nil instead of throw an exception

--- 01.scala
+++ 01.scala
@@ -4,9 +4,9 @@
 //def last(l:List[Any]) = l takeRight 1 head
 
 def last(l:List[Any]) : Any = l match {
+  case Nil => Nil
   case h :: Nil => h
   case h :: t => last(t)
-  case _ => throw new NoSuchElementException
 }
 
 println(last(List(1, 1, 2, 3, 5, 8)))
--- 02.scala
+++ 02.scala
@@ -6,7 +6,7 @@
 def penultimate(l:List[Any]) : Any = l match {
   case h :: (_ :: Nil) => h
   case h :: t => penultimate(t)
-  case _ => throw new NoSuchElementException
+  case _ => Nil
 }
 
 println(penultimate(List(1, 1, 2, 3, 5, 8)))
